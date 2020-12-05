1a1b67acbd574b5c91c4eb5916e1a21fcaaa8e5a
nbernard3 <nicolas.bernard3@gmail.com>
2020-12-05 19:18:47 +0100

    Using vue to manage button

--- pushups.js
+++ pushups.js
@@ -4,51 +4,79 @@
 // the link to your model provided by Teachable Machine export panel
 const URL = "./model/"; // ne peut-être assigné qu'une fois
 const bufferSize = 10;
-let model, webcam, ctx; // scope limité à ce script
 
-var repsCounter = new Vue({
-    el: "#reps-counter",
-    data: {
-        iReps: 0,
-        labels: [],
-        fps: 0.0,
-        tPrevious: 0.0,
-    },
+const startbutton = {
     methods: {
-        init: init
-    },
-})
+        start() {
+            console.info("button clicked");
+            startCamera();
+        }
+    }
+}
 
-async function init() {
-    const modelURL = URL + "model.json";
-    const metadataURL = URL + "metadata.json";
+Vue.createApp(startbutton).mount("#start-button");
+
+const cameraview = document.querySelector("#camera-view");
+
+function startCamera() {
+    var constraints = { video: { facingMode: "user" }, audio: false };
+    navigator.mediaDevices.getUserMedia(constraints).then(function (stream) {
+        track = stream.getTracks()[0];
+        cameraview.srcObject = stream;
+    }).catch(function (error) {
+        console.error("Oops. Something is broken.", error);
+    });
+}
+
+
+function startPosenet() {
+
+    // let model, webcam, ctx; // scope limité à ce script
+    // new Vue({
+    //     el: "#reps-counter",
+    //     data: {
+    //         iReps: 0,
+    //         labels: [],
+    //         fps: 0.0,
+    //         tPrevious: 0.0,
+    //     },
+    //     methods: {
+    //         start: start
+    //     },
+    // })
+
+    // const modelURL = URL + "model.json";
+    // const metadataURL = URL + "metadata.json";
 
     // load the model and metadata
     // Note: the pose library adds a tmPose object to your window (window.tmPose)
-    model = await tmPose.load(
-        modelURL,
-        metadataURL);
+    // model = await tmPose.load(
+    //     modelURL,
+    //     metadataURL);
 
-    model.getClassLabels().forEach(
-        el => repsCounter.labels.push({
-            name: el,
-            probability: new Array(bufferSize).fill(0),
-            filteredProbability: 0,
-        }))
+    // model.getClassLabels().forEach(
+    //     el => repscounter.labels.push({
+    //         name: el,
+    //         probability: new Array(bufferSize).fill(0),
+    //         filteredProbability: 0,
+    //     }))
 
-    // Convenience function to setup a webcam
-    const size = 200;
-    const flip = true; // whether to flip the webcam
-    webcam = new tmPose.Webcam(size, size, flip); // width, height, flip
-    await webcam.setup(); // request access to the webcam
-    await webcam.play();
-    window.requestAnimationFrame(loop);
+    // // Convenience function to setup a webcam
+    // const size = 200;
+    // const flip = true; // whether to flip the webcam
+    // webcam = new tmPose.Webcam(size, size, flip); // width, height, flip
+    // await webcam.setup(); // request access to the webcam
+    // await webcam.play();
+    // window.requestAnimationFrame(loop);
 
-    // append/get elements to the DOM
-    const canvas = document.getElementById("posenet-viz");
-    canvas.width = size; canvas.height = size;
-    ctx = canvas.getContext("2d");
+    // // append/get elements to the DOM
+    // const canvas = document.getElementById("posenet-viz");
+    // canvas.width = size; canvas.height = size;
+    // ctx = canvas.getContext("2d");
 }
+
+
+const posenetviz = document.querySelector("#posenet-viz");
 
 async function loop(timestamp) {
     webcam.update(); // update the webcam frame
@@ -56,8 +84,8 @@ async function loop(timestamp) {
     window.requestAnimationFrame(loop);
 
     tNow = performance.now();
-    repsCounter.fps = (1000. / (tNow - repsCounter.tPrevious)).toFixed(1);
-    repsCounter.tPrevious = tNow;
+    repscounter.fps = (1000. / (tNow - repscounter.tPrevious)).toFixed(1);
+    repscounter.tPrevious = tNow;
 }
 
 async function predict() {
@@ -68,16 +96,17 @@ async function predict() {
     const prediction = await model.predict(posenetOutput);
 
     for (let i = 0; i < prediction.length; i++) {
-        probability = repsCounter.labels[i].probability;
+        probability = repscounter.labels[i].probability;
         probability.shift();
         probability.push(prediction[i].probability);
 
-        repsCounter.labels[i].probability = probability;
-        repsCounter.labels[i].filteredProbability = probability.reduce((total, el) => total + el, 0) / probability.length;
+        repscounter.labels[i].probability = probability;
+        repscounter.labels[i].filteredProbability = probability.reduce((total, el) => total + el, 0) / probability.length;
     }
 
     drawPose(pose);
 }
+
 
 function drawPose(pose) {
     if (webcam.canvas) {
@@ -89,3 +118,6 @@ function drawPose(pose) {
         }
     }
 }
+
+
+
